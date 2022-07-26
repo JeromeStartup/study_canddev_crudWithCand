f8d5e9b255d86fd4c7c4ae8e72106da963bcb8c1
unknown <startup.jerome@gmail.com>
2022-07-26 10:15:10 +0530

    Retrieved data from firebase

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-const btnAdd=document.querySelector('.btn-add');
+const btnAdd = document.querySelector('.btn-add');
 // modal add
-const addModal=document.querySelector('.add-modal');
+const addModal = document.querySelector('.add-modal');
+
+const tableUsers = document.querySelector('.table-users');
+
+//Create element and render users
+const renderUser = doc => {
+    const tr = `
+       <tr>
+           <td>${doc.data().firstName}</td>
+           <td>${doc.data().lastName}</td>
+           <td>${doc.data().phone}</td>
+           <td>${doc.data().email}</td>
+           <td>
+               <button class="btn btn-edit">Edit</button>
+               <button class="btn btn-delete">Delete</button>
+           </td>
+       </tr>`
+       tableUsers.insertAdjacentHTML('beforeend',tr);
+}
+
+
 // click add user button
-btnAdd.addEventListener('click',()=>{
+btnAdd.addEventListener('click', () => {
     addModal.classList.add("modal-show")
 })
 
 // User click anywhere out
-window.addEventListener('click',e=>{
+window.addEventListener('click', e => {
     console.log(e.target)
-    if(e.target == addModal){
+    if (e.target == addModal) {
         addModal.classList.remove('modal-show');
     }
+})
+
+// get all users
+db.collection("users").get().then(querySnapshot => {
+    querySnapshot.forEach(doc => {
+        console.log(doc.data());
+        renderUser(doc);
+    });
+
 })
